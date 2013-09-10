a20ce106258281fd4f41d77a4c743bb650e7fadd
ZAHIM Anas <zanass0@gmail.com>
2013-09-10 23:44:34 +0100

    Adding User.facebook_picture()

--- pinry/users/models.py
+++ pinry/users/models.py
@@ -8,5 +8,12 @@ class User(BaseUser):
     def gravatar(self):
         return hashlib.md5(self.email).hexdigest()
 
+    def facebook_picture(self):
+        try:
+       	   fid = self.social_auth.filter(provider="facebook")[0].uid
+        except:
+	   return
+	return "https://graph.facebook.com/" + fid + "/picture?type=large"
+
     class Meta:
         proxy = True
